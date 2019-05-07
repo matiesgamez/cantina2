939642d3ab910817ce9538105e7d55a57e9dcc2c
alumnat <alumnat@DESKTOP-KS8A00N>
2019-05-07 12:23:16 +0200

    inici

---
+++ projecte/registre.php
@@ -0,0 +1,116 @@
+<?php
+$servername = "db5000047478.hosting-data.io";
+$database = "dbs42384";
+$username = "dbu113975";
+$password = "prueb@sTIC006";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      $nom=$_POST['nom'];
+      $llinatges=$_POST['llinatges'];
+      $user=$_POST['username'];
+      $password=$_POST['password'];
+
+      $sql = "INSERT INTO PROVA_USUARI (NOM, LLINATGES, USUARI, CONTRASENYA)
+      VALUES ('$nom', '$llinatges', '$user', '$password')";
+
+      if ($conn->query($sql) === TRUE) {
+          header("location: usuaris.php");
+      } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+
+      $conn->close();
+   }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Pàgina Web de prova TIC</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/one-page-wonder.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Pàgina Web de prova TIC</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="registre.php">Enregistra't</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="login.php">Entra</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="masthead text-center text-white">
+    <div class="masthead-content">
+      <div class="container">
+        <form action="registre.php" method="post">
+            <input type="text" name="nom" class="btn btn-secondary btn-xl rounded-pill mt-5" placeholder="Nom" /> <br>
+            <input type="text" name="llinatges" class="btn btn-secondary btn-xl rounded-pill mt-5" placeholder="Llinatges" /> <br>
+            <input type="text" name="username" class="btn btn-secondary btn-xl rounded-pill mt-5" placeholder="usuari" /> <br>
+            <input type="password" name="password" class="btn btn-secondary btn-xl rounded-pill mt-5" placeholder="Contrasenya" /> <br>
+            <input type="submit" class="btn btn-primary btn-xl rounded-pill mt-5" value="Enregistra't" />
+            <?php echo $error; ?>
+        </form>
+      </div>
+    </div>
+    <div class="bg-circle-1 bg-circle"></div>
+    <div class="bg-circle-2 bg-circle"></div>
+    <div class="bg-circle-3 bg-circle"></div>
+    <div class="bg-circle-4 bg-circle"></div>
+  </header>
+
+
+  <!-- Footer -->
+  <footer class="py-5 bg-black">
+    <div class="container">
+      <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
