570464b7deb6c3764abfb87b8358b839078c2a7d
alumnat <alumnat@DESKTOP-KS8A00N>
2019-05-10 10:57:55 +0200

    inici

--- signup/signup.php
+++ signup/signup.php
@@ -29,7 +29,7 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <!--===============================================================================================-->
 </head>
-<body style="background-color: #999999;">
+<body style="">
 	
 	<div class="limiter">
 		<div class="container-login100">
